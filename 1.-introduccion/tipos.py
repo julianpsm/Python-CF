a76fb0a7b74387234ed9a3de35eb5d84d51cab72
Teddy <edisonpaulcrz@gmail.com>
2021-10-28 00:14:37 -0500

    Tipos de Datos

---
+++ 1.-introduccion/tipos.py
@@ -0,0 +1,34 @@
+# String
+titulo_curso = 'Curso Profesional de Python'
+print(titulo_curso)
+
+nombre_completo = "Just Code Developer"
+print(nombre_completo)
+
+mensaje = "'CódigoFacilito'"
+print(mensaje)
+mensaje2 = '"CódigoFacilito"'
+print(mensaje2)
+mensaje3 = '''Te encuentras en el curso:
+Profesional de Python.
+En Código Facilito'''
+print(mensaje3)
+
+# Int
+
+# Al dividir con // obtenemos tipo de dato entero
+numero_uno = 10 // 3
+print(numero_uno)
+
+# Float
+# Al dividir con / obtenemos tipo de dato flotante
+numero_uno = 10 / 3
+print(numero_uno)
+
+numero_dos = -3.14
+print(numero_dos)
+
+# Bool
+# True - False
+valor = True
+print(valor)
