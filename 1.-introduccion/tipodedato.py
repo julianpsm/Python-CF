b05c57d799e08c68e0d28565ed314a54b190877a
Teddy <edisonpaulcrz@gmail.com>
2021-10-28 01:06:01 -0500

    type

---
+++ 1.-introduccion/tipodedato.py
@@ -0,0 +1,15 @@
+valor = "Justcoddev"
+tipo = type(valor)
+print(tipo)
+
+valor = 2
+tipo = type(valor)
+print(tipo)
+
+valor = 3.1
+tipo = type(valor)
+print(tipo)
+
+valor = True
+tipo = type(valor)
+print(tipo)
