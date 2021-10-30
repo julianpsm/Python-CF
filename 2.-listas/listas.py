6ce8261f85234cec237286e82a2964c5d86f7def
Teddy <edisonpaulcrz@gmail.com>
2021-10-29 20:51:57 -0500

    Listas!

---
+++ 2.-listas/listas.py
@@ -0,0 +1,13 @@
+# se leen de izq a der, pero si son número negativos la lectura será de der a izq
+#                   0          1        2       3       4
+#                   -5        -4       -3      -2      -1
+lista_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
+
+primer_curso = lista_cursos[0]
+print(primer_curso)
+
+segundo_curso = lista_cursos[1]
+print(segundo_curso)
+
+ultimo_curso = lista_cursos[4]
+print(ultimo_curso)
