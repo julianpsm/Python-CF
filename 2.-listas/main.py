48ba38c6c146efc75f6cd5ff70a60dac0f8622a3
Teddy <edisonpaulcrz@gmail.com>
2021-10-29 20:33:01 -0500

    ¿Qué son listas?

---
+++ 2.-listas/main.py
@@ -0,0 +1,7 @@
+lista_strings = ["Teddy", "Python", "Código"]
+
+lista_enteros = [-19, 0, 10, 200]
+
+lista_floats = [19.5, -20.5, 80.99, 100.00]
+
+lista_booleanos = {True, True, (2 < 10), (4 > 3 and 10 != 11)}
