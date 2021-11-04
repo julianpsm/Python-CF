e352108f51720349d79eeabd8e58eda52bf6fe2e
justcoddev <edisonpaulcrz@gmail.com>
2021-11-04 00:31:40 -0500

    Métodos

--- 2.-listas/metodos.py
+++ 2.-listas/metodos.py
@@ -1,5 +1,16 @@
 lista = [8, 90, 1, 5, 44, 132, 600, 3, 4]
 
-#ordenar de mayor a menor
+#ordenar de menor a mayor
 lista.sort();
 print(lista)
+
+#ordenar de mayor a menor
+lista.sort(reverse = True);
+print(lista)
+
+#min  -   max
+print(min(lista))
+print(max(lista))
+
+#buscar elemento
+print(5 in lista)
