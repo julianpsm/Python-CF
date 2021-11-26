1a210fdef7316658dbd9169372a00799a3f62125
justcoddev <edisonpaulcrz@gmail.com>
2021-11-25 23:41:54 -0500

    obtener elemento tuplas-subtupla

---
+++ 3.-Tuplas/tuplas_indice.py
@@ -0,0 +1,13 @@
+cursos = ('Python', 'Fask', 'Django','Rails', 'MongoDB')
+
+#Obtener un elemento de la tupla
+primer_curso= cursos[0]
+print(primer_curso)
+
+#Obtener un elemento de la tupla
+ultimo_curso= cursos[-1]
+print(ultimo_curso)
+
+#SUBTUPLA
+sub_tupla = cursos[0:3]
+print(sub_tupla)
