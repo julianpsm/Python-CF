08f514942eeabf31dc6ded93ece868902923d4da
justcoddev <edisonpaulcrz@gmail.com>
2021-11-25 23:48:36 -0500

    listas a tuplas y viseversa

---
+++ 3.-Tuplas/listas_tuplas.py
@@ -0,0 +1,12 @@
+#lista
+cursos = ['Python', 'Flask', 'Django']
+#pasar lista a tupla
+cursos_tupla = tuple(cursos)
+print(type(cursos_tupla))
+
+#tupla
+niveles = ('Básico', 'Intermedio', 'Avanzado')
+
+#lista a tupla
+niveles_lista = list(niveles)
+print(type(niveles_lista))
