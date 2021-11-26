b1dc34f8c735c9dc552ec26132fce8fee65228fe
justcoddev <edisonpaulcrz@gmail.com>
2021-11-25 22:58:55 -0500

    sublistas

--- 2.-listas/sublistas.py
+++ 2.-listas/sublistas.py
@@ -1,6 +1,9 @@
 lista_cursos = ['Python', 'Django', 'Flask', 'Ruby', 'Java', 'Rust']
 #cambio
 # [start:end]
+# [start:]- > Obtenemos los últimos elementos de la lista
+# [:end]-> Obtenemos los primeros elementos de la lista
+# [start:end:skip]
 sub_lista = lista_cursos[0:3]
 sub_lista = sub_lista[1:5:2]
 print(sub_lista)
