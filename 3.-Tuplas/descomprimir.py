fabeb7bad7862be4845ca6b161d3412c82719e11
justcoddev <edisonpaulcrz@gmail.com>
2021-11-26 15:42:17 -0500

    descomprimir

--- 3.-Tuplas/descomprimir.py
+++ 3.-Tuplas/descomprimir.py
@@ -7,8 +7,9 @@ print(tres)
 print(cuatro)
 
 #ahora con tuplas
-numeros = (1, 2, 3, 4, 5, 6)
-uno, dos, tres, cuatro, * resto_valores = numeros
+numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+uno, dos, tres, cuatro, *resto_valores, nueve, diez  = numeros
+# * resto_valores
 #cuando no son asignados a variables se los muestra así, se da cuenta python
 #creando nueva lista colocando el * como prefijo, ya que son valores que n ose desenpaquetan
 # y si no deseamos trabajar con esos valores simplemente se coloca lo siguiente
@@ -17,6 +18,8 @@ print(uno)
 print(dos)
 print(tres)
 print(cuatro)
+# print(resto_valores)
+
+print(nueve)
+print(diez)
 print(resto_valores)
-
-
