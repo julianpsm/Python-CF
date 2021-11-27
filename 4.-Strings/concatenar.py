4abc950b65cfae9f9a70004d8fb1f5082b75f4cf
justcoddev <edisonpaulcrz@gmail.com>
2021-11-27 00:08:18 -0500

    concatenar

--- 4.-Strings/concatenar.py
+++ 4.-Strings/concatenar.py
@@ -1,2 +1,9 @@
 nombre = 'Teddy'
 apellido = 'Summers'
+nombre_completo = nombre+" "+apellido
+print(nombre_completo)
+#usando %s
+
+nombre_completo = 'Mr. %s %s %s' %(nombre, apellido, "justcoddev")
+
+print(nombre_completo)
