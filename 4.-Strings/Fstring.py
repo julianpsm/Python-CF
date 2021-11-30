d2368779e6f232a944cf7761a2ae9084a7fc4ae5
justcoddev <edisonpaulcrz@gmail.com>
2021-11-29 22:46:09 -0500

    interpolacion-Fstring

---
+++ 4.-Strings/Fstring.py
@@ -0,0 +1,10 @@
+nombre = 'Teddy'
+apellido = 'Summers'
+
+
+nombre_completo= f'Mr. {nombre} {apellido} {"justcoddev"}'
+print(nombre_completo)
+
+
+nombre_completo= f'Mr. {nombre} {apellido} {10*20}'
+print(nombre_completo)
